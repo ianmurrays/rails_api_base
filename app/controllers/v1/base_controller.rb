849436040d536f14ec9d692de15e4fbbd01d94dd
Ian Murray <ianmurrays@gmail.com>
2014-06-11 22:40:18 -0400

    Authorization header for authentication

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -1,3 +1,21 @@
 class V1::BaseController < ApplicationController 
   respond_to :json
+
+  before_filter :authenticate!
+
+  helper_method :current_user
+
+private
+
+  def authenticate!
+    token = request.headers['Authorization']
+
+    unless @current_user = AuthenticationToken.where(['token = ? AND expires_at > ?', token, Time.now]).first.try(:user)
+      head :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
+  end
 end
